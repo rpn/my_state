790377fa770ae199506819a013f91f4fa27ba8c0
rokumoto <6081326+rpn@users.noreply.github.com>
2026-04-06 11:36:16 +0900

    my first commit

---
+++ src/test/test_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <gtest/gtest.h>
+
+#include <memory>
+#include <string>
+#include <cctype>
+#include <stdexcept>
+#include <sstream>
+
+namespace test_1 {
+
+TEST(Test1, test1) {
+    ASSERT_TRUE(false);
+}
+
+} // namespace test_1
