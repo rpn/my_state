ae4f75b7f49986fb1213437bb3a4caba13a7e544
rokumoto <6081326+rpn@users.noreply.github.com>
2026-04-08 13:47:15 +0900

    feat: etc...

--- src/test/simple_enemy_state_1.cpp
+++ src/test/simple_enemy_state_1.cpp
@@ -6,6 +6,12 @@ namespace simple_enemy_state_1 {
 
 enum class ShooterView {
 	int post_address = 0;
+
+#if NOTE
+	bool open_post_box();
+		// 開設
+	void close_post_box();
+#endif
 };
 
 #if NOTE
@@ -14,6 +20,13 @@ enum class ShooterView {
 - ハンドルは単純なタイプである必要がある(intとか)
 - しかしコールバックは引数付き
 - ポストからコールバックを取り出す関数が必要(昔作ったような・・・)
+- 受け取りがなかったらどうなる？キューにたまり続ける？
+
+開設したら届くようになる
+	- 開設以前のイベントが届かないのは困る場合は？
+		- 強制開設があってもいいかも？
+	- 複数の相手に同時に届けるとかできるの？
+		- 便利関数としては用意するかも
 
 - torelloにいっぱいタスクを作ろう
 - 会社用のwebサービスって簡単に作れる？
