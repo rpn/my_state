6f6b1481fed02d06a675beadbd1a9f079113efe3
rpn <6081326+rpn@users.noreply.github.com>
2026-04-13 09:49:03 +0900

    Update my_sml_1.cpp

--- src/test/my_sml_1.cpp
+++ src/test/my_sml_1.cpp
@@ -8,11 +8,13 @@ namespace sml = boost::sml;
 struct on_update {
     float delta_time;
     float input_power;
+    bool jump = false;
 };
 
 auto is_idle    = [](const on_update& e) { return e.input_power < 0.1f; };
 auto is_walking = [](const on_update& e) { return e.input_power >= 0.1f && e.input_power < 0.8f; };
 auto is_running = [](const on_update& e) { return e.input_power >= 0.8f; };
+auto is_jumping = [](const on_update& e) { return e.jump == true; };
 
 struct character_movement {
     auto operator()() const {
@@ -22,11 +24,16 @@ struct character_movement {
         	*"idle"_s + event<on_update> [is_walking] = "walk"_s,
         	"idle"_s + event<on_update> [is_running] = "run"_s,
 
+        	"walk"_s + event<on_update> [is_jumping] = "jump"_s,
         	"walk"_s + event<on_update> [is_running] = "run"_s,
         	"walk"_s + event<on_update> [is_idle] = "idle"_s,
 
+        	"run"_s + event<on_update> [is_jumping] = "jump"_s,
         	"run"_s + event<on_update> [is_walking] = "walk"_s,
-        	"run"_s + event<on_update> [is_idle] = "idle"_s
+        	"run"_s + event<on_update> [is_idle] = "idle"_s,
+
+        	"idle"_s + event<on_update> [is_jumping] = "jump"_s,
+        	"jump"_s + event<on_update> [is_idle] = "idle"_s
         );
     }
 };
@@ -34,8 +41,38 @@ struct character_movement {
 TEST(MySml1, test1)
 {
     using sml::literals::operator""_s;
+	sml::sm<character_movement> sm;
 
-    ASSERT_TRUE(false);
+	ASSERT_TRUE(sm.is("idle"_s));
+
+	sm.process_event(on_update{1.0f, 0.0f});
+	ASSERT_TRUE(sm.is("idle"_s));
+
+	sm.process_event(on_update{1.0f, 0.1f});
+	ASSERT_TRUE(sm.is("walk"_s));
+
+	sm.process_event(on_update{1.0f, 0.0f});
+	ASSERT_TRUE(sm.is("idle"_s));
+
+	sm.process_event(on_update{1.0f, 1.0f});
+	ASSERT_TRUE(sm.is("run"_s));
+
+	sm.process_event(on_update{1.0f, 0.2f});
+	ASSERT_TRUE(sm.is("walk"_s));
+
+	sm.process_event(on_update{1.0f, 0.0f});
+	ASSERT_TRUE(sm.is("idle"_s));
+
+	sm.process_event(on_update{1.0f, 0.0f, true});
+	ASSERT_TRUE(sm.is("jump"_s));
+	sm.process_event(on_update{1.0f, 0.0f});
+	ASSERT_TRUE(sm.is("idle"_s));
+
+	sm.process_event(on_update{1.0f, 1.0f});
+	ASSERT_TRUE(sm.is("run"_s));
+
+	sm.process_event(on_update{1.0f, 1.0f, true});
+	ASSERT_TRUE(sm.is("jump"_s));
 }
 
 } // namespace my_sml_1
